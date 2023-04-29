170dc86fbf4577d552f06630a729a931c70a595b
Ben De Cruz <decruz_ben@hotmail.com>
2023-04-29 14:40:33 +1000

    url fun

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route index element={<BasicChatApp />} />
+        <Route path="/" element={<BasicChatApp />} />
         <Route path="BasicChatApp" element={<BasicChatApp />} />
         <Route path="BasicChatApp/Login" element={<Login />} />
       </Routes>
